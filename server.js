bbf95023fdf5286548e1372f0cdc33fa6728214b
musfiqur224-cell <musfiqur224@gmail.com>
2026-04-08 15:26:59 +0600

    Fix syntax error in server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ async function seedConfig() {
 }
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));ask: "", pay: "", prof: "" }
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
         });
     }
 }
